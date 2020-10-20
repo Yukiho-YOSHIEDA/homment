90ac93e9f008f6956c1ba49fb03fc4fd0668975f
yukiho <is0436er@ed.ritsumei.ac.jp>
2020-10-20 18:01:14 +0900

    [Add] プロジェクトを作った

---
+++ src/main/java/dev/yukiho/homment/HommentApplication.java
@@ -0,0 +1,13 @@
+package dev.yukiho.homment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HommentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/yukiho/homment/HommentApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yukiho.homment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
