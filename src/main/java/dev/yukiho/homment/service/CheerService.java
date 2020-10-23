49d45bab1b7c075d0c0da3cc32c1cd12e956e656
yukiho <is0436er@ed.ritsumei.ac.jp>
2020-10-24 04:45:23 +0900

    refs #39 APIの実装

---
+++ src/main/java/dev/yukiho/homment/service/CheerService.java
@@ -0,0 +1,38 @@
+package dev.yukiho.homment.service;
+
+import dev.yukiho.homment.db.model.Cheer;
+import dev.yukiho.homment.repository.CheerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 応援コメントに関するサービスです
+ */
+@Service
+public class CheerService {
+
+    private final CheerRepository cheerRepository;
+
+    @Autowired
+    public CheerService(CheerRepository cheerRepository) {
+        this.cheerRepository = cheerRepository;
+    }
+
+    /**
+     * 応援コメントを保存します
+     *
+     * @param action
+     * @param userId
+     * @param roomId
+     */
+    public void save(String action, int userId, int roomId) {
+        final var cheer = new Cheer();
+        cheer.setComment(action);
+        cheer.setUserId(userId);
+        cheer.setRoomId(roomId);
+
+        cheerRepository.insert(cheer);
+
+        // TODO: ブロードキャスト
+    }
+}
