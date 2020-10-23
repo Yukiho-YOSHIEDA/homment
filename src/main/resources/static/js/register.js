bd48d5cb5d1a0ef9e7383c227445c0720ee71dff
yukiho <is0436er@ed.ritsumei.ac.jp>
2020-10-23 23:44:07 +0900

    refs #21 動的にアイコンを変えた

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,5 +1,7 @@
 const nameTextBox = document.getElementById('nameText');
 const postButton = document.getElementById('postButton');
+const imageInput = document.getElementById('imageInput');
+const icon = document.getElementById('icon');
 
 nameTextBox.addEventListener('keyup', function () {
   if (!nameTextBox.value) {
@@ -8,3 +10,18 @@ nameTextBox.addEventListener('keyup', function () {
   }
   postButton.disabled = false;
 });
+
+imageInput.addEventListener('change', function (event) {
+  updateIcon(event.target.files[0]);
+});
+
+function updateIcon(image) {
+  icon.file = image;
+  const reader = new FileReader();
+  reader.onload = (function (img) {
+    return function (e) {
+      img.src = e.target.result;
+    };
+  })(icon);
+  reader.readAsDataURL(image);
+}
