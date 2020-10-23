0e38df2a290ca520fd3c5b9a59bec5164c336580
yukiho <is0436er@ed.ritsumei.ac.jp>
2020-10-24 03:04:54 +0900

    refs #37 DB壊した

--- src/main/resources/db/migration/V0_0_2__create_room.sql
+++ src/main/resources/db/migration/V0_0_2__create_room.sql
@@ -3,5 +3,12 @@ CREATE TABLE IF NOT EXISTS `room`
     `id`         INT          NOT NULL AUTO_INCREMENT,
     `name`       VARCHAR(128) NOT NULL,
     `created_at` DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+    `created_by` INT          NOT NULL,
+    PRIMARY KEY (`id`),
+    INDEX `fk_room_user1_idx` (`created_by` ASC) VISIBLE,
+    CONSTRAINT `fk_room_user1`
+        FOREIGN KEY (`created_by`)
+            REFERENCES `user` (`id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
 );
